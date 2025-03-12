97c6b3df832b517ee1e03e68040ca86c52d704e2
Keerthi Kumar <n.keerthikumar85@gmail.com>
2025-03-12 10:59:27 +0530

    started to implement open telemetry with springboot

---
+++ src/main/java/com/opentelemetry/springboot/OpentelemetrySpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.opentelemetry.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpentelemetrySpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpentelemetrySpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/opentelemetry/springboot/OpentelemetrySpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opentelemetry.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpentelemetrySpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
